ab6c0522f8219076c3b83404a9372c4b4c05e4ca
Tom Ross <tom.ross@protonmail.com>
2021-05-18 18:05:17 +0800

    [ADD]: Create SLURM script for GOL CUDA jobs

---
+++ ex1-gol-cuda/jobs.sh
@@ -0,0 +1,24 @@
+#!/bin/bash --login
+
+# load modules
+module load gcc/8.3.0 cuda/10.2
+
+# parameters
+nsteps="100"
+n_set="1 2 4 8 16 32 64 128 256 512 1024 2048 4096 8192 16384"
+
+# submit jobs
+echo "GOL (cpu|cuda) jobs"
+echo "> nsteps: ${nsteps}"
+echo "> n_set: ${n_set}"
+
+for n in ${n_set}
+do
+    echo "> submitting (cpu|cuda) jobs for n=${n}"
+
+    # submit cpu job
+    sbatch --export="n=${n},m=${n},nsteps=${nsteps}" cpu.slurm
+
+    # submit gpu job
+    sbatch --export="n=${n},m=${n},nsteps=${nsteps}" gpu.slurm
+done

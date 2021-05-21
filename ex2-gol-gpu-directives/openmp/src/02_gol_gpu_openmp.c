c7d7849098c08ce0b88b62586432534df7e3e1ce
Tom Ross <tom.ross@protonmail.com>
2021-05-21 12:28:25 +0800

    [FIX]: Tune OpenMP GPU code

--- ex2-gol-gpu-directives/openmp/src/02_gol_gpu_openmp.c
+++ ex2-gol-gpu-directives/openmp/src/02_gol_gpu_openmp.c
@@ -239,23 +239,16 @@ int main(int argc, char **argv)
   // - `enter data` defines a transfer of cpu memory to gpu memory
   // - `map(to:*)` moves `grid` and `updated_grid` from the cpu memory into the
   //   gpu memory
-  #pragma omp target enter data map(to:grid[0:n*m],updated_grid[0:n*m])
+#pragma omp target enter data map(to:grid[0:n*m],updated_grid[0:n*m])
 
   // calculate final game_of_life state
-  //#pragma omp target data map(tofrom:grid[0:n*m],updated_grid[0:n*m])
   while (current_step != nsteps)
   {
 #pragma omp single
     kernel_start = init_time();
 
-#pragma omp single
-    printf("step %i : %f ms\n", current_step, get_elapsed_time(start));
-
-#pragma omp single
-    printf("> 1  : %f ms\n", current_step, get_elapsed_time(kernel_start));
-
     // perform game_of_life step (in-line for debugging)
-#pragma omp target teams distribute parallel for collapse(2) schedule(static, 1)
+#pragma omp target teams distribute parallel for collapse(2) schedule(static,1)
     for (int i = 0; i < n; i++)
     {
       for (int j = 0; j < m; j++)
@@ -268,14 +261,8 @@ int main(int argc, char **argv)
     }
     // game_of_life(opt, grid, updated_grid, n, m);
 
-#pragma omp single
-    printf("> 2a : %f ms\n", current_step, get_elapsed_time(kernel_start));
-
     // wait for all cells to be updated
 #pragma omp barrier
-
-#pragma omp single
-    printf("> 2b : %f ms\n", current_step, get_elapsed_time(kernel_start));
 
 #pragma omp single
     kernel_time += get_elapsed_time(kernel_start);
@@ -289,9 +276,6 @@ int main(int argc, char **argv)
       updated_grid = tmp;
     }
 
-#pragma omp single
-    printf("> 3  : %f ms\n", current_step, get_elapsed_time(kernel_start));
-
     // wait before re-entering loop
 #pragma omp barrier
 
@@ -304,7 +288,7 @@ int main(int argc, char **argv)
   // - `exit data` defines a transfer of gpu memory to cpu memory
   // - `map(from:*)` moves `grid` and `updated_grid` from the gpu memory into
   //   the cpu memory
-  #pragma omp target exit data map(from:grid[0:n*m],updated_grid[0:n*m])
+#pragma omp target exit data map(from:grid[0:n*m],updated_grid[0:n*m])
 
   // finalise timing and write output
   float elapsed_time = get_elapsed_time(start);

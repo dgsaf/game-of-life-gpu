fd8ec94b065e816de7c291341efe795fe5713110
Tom Ross <tom.ross@protonmail.com>
2021-05-21 10:59:03 +0800

    [FIX]: Inline OpenMP game_of_life step

--- ex2-gol-gpu-directives/openmp/src/02_gol_gpu_openmp.c
+++ ex2-gol-gpu-directives/openmp/src/02_gol_gpu_openmp.c
@@ -224,6 +224,8 @@ int main(int argc, char **argv)
       // perform game_of_life step (in-line for debugging)
       int *tmp = NULL;
       int i, j;
+      int n_i[8], n_j[8];
+      int neighbours = 0;
 
 #pragma omp                                     \
   teams distribute parallel for                 \
@@ -235,8 +237,6 @@ int main(int argc, char **argv)
         for (j = 0; j < m; j++)
         {
           // index the neighbourhood around current cell
-          int n_i[8], n_j[8];
-
           n_i[0] = i - 1; n_j[0] = j - 1;
           n_i[1] = i - 1; n_j[1] = j;
           n_i[2] = i - 1; n_j[2] = j + 1;
@@ -247,24 +247,22 @@ int main(int argc, char **argv)
           n_i[7] = i;     n_j[7] = j - 1;
 
           // count the number of living neighbours
-          int neighbours = 0;
-
-          if (n_i[0] >= 0 && n_j[0] >= 0                                \
-              && current_grid[n_i[0] * m + n_j[0]] == ALIVE) neighbours++;
-          if (n_i[1] >= 0                                                   \
-              && current_grid[n_i[1] * m + n_j[1]] == ALIVE) neighbours++;
-          if (n_i[2] >= 0 && n_j[2] < m                                     \
-              && current_grid[n_i[2] * m + n_j[2]] == ALIVE) neighbours++;
-          if (n_j[3] < m                                                    \
-              && current_grid[n_i[3] * m + n_j[3]] == ALIVE) neighbours++;
-          if (n_i[4] < n && n_j[4] < m                                      \
-              && current_grid[n_i[4] * m + n_j[4]] == ALIVE) neighbours++;
-          if (n_i[5] < n                                                    \
-              && current_grid[n_i[5] * m + n_j[5]] == ALIVE) neighbours++;
-          if (n_i[6] < n && n_j[6] >= 0                                     \
-              && current_grid[n_i[6] * m + n_j[6]] == ALIVE) neighbours++;
-          if (n_j[7] >= 0                                               \
-              && current_grid[n_i[7] * m + n_j[7]] == ALIVE) neighbours++;
+          if (n_i[0] >= 0 && n_j[0] >= 0                            \
+              && grid[n_i[0] * m + n_j[0]] == ALIVE) neighbours++;
+          if (n_i[1] >= 0                                           \
+              && grid[n_i[1] * m + n_j[1]] == ALIVE) neighbours++;
+          if (n_i[2] >= 0 && n_j[2] < m                             \
+              && grid[n_i[2] * m + n_j[2]] == ALIVE) neighbours++;
+          if (n_j[3] < m                                            \
+              && grid[n_i[3] * m + n_j[3]] == ALIVE) neighbours++;
+          if (n_i[4] < n && n_j[4] < m                              \
+              && grid[n_i[4] * m + n_j[4]] == ALIVE) neighbours++;
+          if (n_i[5] < n                                            \
+              && grid[n_i[5] * m + n_j[5]] == ALIVE) neighbours++;
+          if (n_i[6] < n && n_j[6] >= 0                             \
+              && grid[n_i[6] * m + n_j[6]] == ALIVE) neighbours++;
+          if (n_j[7] >= 0                                           \
+              && grid[n_i[7] * m + n_j[7]] == ALIVE) neighbours++;
 
           // update state
           if (grid[i*m + j] == ALIVE && (neighbours == 2 || neighbours == 3))

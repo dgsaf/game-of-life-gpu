b811609b1bf219b123600700281adc2930820321
Tom Ross <tom.ross@protonmail.com>
2021-05-21 10:54:01 +0800

    [FIX]: Tune OpenMP game_of_life parallel for loop

--- ex2-gol-gpu-directives/openmp/src/02_gol_gpu_openmp.c
+++ ex2-gol-gpu-directives/openmp/src/02_gol_gpu_openmp.c
@@ -224,21 +224,61 @@ int main(int argc, char **argv)
       // perform game_of_life step (in-line for debugging)
       int *tmp = NULL;
       int i, j;
-      int neighbours;
 
 #pragma omp                                     \
   teams distribute parallel for                 \
   collapse(2)                                   \
   schedule(static, 1)                           \
-  private(i, j, neighbours)
+  private(i, j, n_i, n_j, neighbours)
       for (i = 0; i < n; i++)
       {
         for (j = 0; j < m; j++)
         {
-          neighbours = game_of_life_neighbours(grid, n, m, i, j);
+          // index the neighbourhood around current cell
+          int n_i[8], n_j[8];
 
-          updated_grid[i*m + j] = game_of_life_next_state(grid[i*m + j], \
-                                                          neighbours);
+          n_i[0] = i - 1; n_j[0] = j - 1;
+          n_i[1] = i - 1; n_j[1] = j;
+          n_i[2] = i - 1; n_j[2] = j + 1;
+          n_i[3] = i;     n_j[3] = j + 1;
+          n_i[4] = i + 1; n_j[4] = j + 1;
+          n_i[5] = i + 1; n_j[5] = j;
+          n_i[6] = i + 1; n_j[6] = j - 1;
+          n_i[7] = i;     n_j[7] = j - 1;
+
+          // count the number of living neighbours
+          int neighbours = 0;
+
+          if (n_i[0] >= 0 && n_j[0] >= 0                                \
+              && current_grid[n_i[0] * m + n_j[0]] == ALIVE) neighbours++;
+          if (n_i[1] >= 0                                                   \
+              && current_grid[n_i[1] * m + n_j[1]] == ALIVE) neighbours++;
+          if (n_i[2] >= 0 && n_j[2] < m                                     \
+              && current_grid[n_i[2] * m + n_j[2]] == ALIVE) neighbours++;
+          if (n_j[3] < m                                                    \
+              && current_grid[n_i[3] * m + n_j[3]] == ALIVE) neighbours++;
+          if (n_i[4] < n && n_j[4] < m                                      \
+              && current_grid[n_i[4] * m + n_j[4]] == ALIVE) neighbours++;
+          if (n_i[5] < n                                                    \
+              && current_grid[n_i[5] * m + n_j[5]] == ALIVE) neighbours++;
+          if (n_i[6] < n && n_j[6] >= 0                                     \
+              && current_grid[n_i[6] * m + n_j[6]] == ALIVE) neighbours++;
+          if (n_j[7] >= 0                                               \
+              && current_grid[n_i[7] * m + n_j[7]] == ALIVE) neighbours++;
+
+          // update state
+          if (grid[i*m + j] == ALIVE && (neighbours == 2 || neighbours == 3))
+          {
+            updated_grid[i*m + j] = ALIVE;
+          }
+          else if (grid[i*m + j] == DEAD && neighbours == 3)
+          {
+            updated_grid[i*m + j] = ALIVE;
+          }
+          else
+          {
+            updated_grid[i*m + j] = DEAD;
+          }
         }
       }
       // game_of_life(opt, grid, updated_grid, n, m);
